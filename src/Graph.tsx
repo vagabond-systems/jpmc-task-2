776caaa260e2f77f7e38357cf3d1d846b1eafce7
Daniil Kucalov <64499176+daniilkucalov@users.noreply.github.com>
2022-10-14 22:56:34 +0100

    Graph automatically updates

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,8 +49,16 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute("view", "y_line"); // makes the kind of graph a y_line, which is used because we need a continuous line
+      elem.setAttribute("column-pivots", '["stock"]'); // allows to separate 2 different stocks
+      elem.setAttribute("row_pivots", '["timestamp"]'); // allows to map each datapoint on the x-axis
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute("aggregates",
+                '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+            );
+        }
     }
-  }
+
 
   componentDidUpdate() {
     // Everytime the data props is updated, insert the data into Perspective table
