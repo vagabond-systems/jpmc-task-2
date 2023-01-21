a1775f42a18b8c3acbaded2e6c627bac24c7a5df
VASIGARANV <120090150+VASIGARANV@users.noreply.github.com>
2023-01-21 08:23:41 +0530

    task-2 App.tsx.updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+    showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,20 +31,31 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if(this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
   }
-
+  } 
   /**
    * Get new data from server and update the state with the new data
    */
-  getDataFromServer() {
+getDataFromServer() { 
+  let x = 0;
+  const interval = setInterval(() => { 
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
+       this.setState({
+         data: serverResponds,
+         showGraph: true,
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
     });
-  }
-
+  });
+   x++;
+ if (x > 1000) {
+   clearInterval(interval);
+ }
+}, 100);
+}
   /**
    * Render the App react component
    */
@@ -54,6 +67,8 @@ class App extends Component<{}, IState> {
         </header>
         <div className="App-content">
           <button className="btn btn-primary Stream-button"
+            <div className="Graph">
+            {this.renderGraph()}
             // when button is click, our react app tries to request
             // new data from the server.
             // As part of your task, update the getDataFromServer() function
