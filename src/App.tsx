0e48fee1a592357a5d9368fb879704b4663715af
Ayan Chakraborty <ac4462383@gmail.com>
2023-01-31 17:45:15 +0530

    modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean
 }
 
 /**
@@ -22,6 +23,9 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // showGraph is a flag that controls whether the Graph element should be rendered or not
+      // We use this state to toggle the Graph element on and off
+      showGraph: false
     };
   }
 
@@ -29,18 +33,28 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if (this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
   }
+}
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x=0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+          this.setState({
+              data: serverResponds,
+              showGraph: true
+          });
+      });
+      x++;
+      if (x > 1000) {
+          clearInterval(interval);
+      }
+  }, 100);
   }
 
   /**
