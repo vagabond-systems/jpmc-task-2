bbf7c06f1bbef4f7f7d05bc818dc56daabf407a7
Krishnagcp <krishnaggkbt@gmail.com>
2023-03-12 14:23:36 +0530

    modified:   src/Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -53,11 +53,8 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('column-pivots','["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates',
-        {"stock" : "distinct count",
-        "top_ask_price" : "avg",
-        "top_bid_price" : "avg",
-        "timestamp" : "distinct count"});
+      elem.setAttribute("aggregates",
+        '{"stock" : "distinct count", "top_ask_price" : "avg","top_bid_price" : "avg","timestamp" : "distinct count"}');
        }
     }
   }
